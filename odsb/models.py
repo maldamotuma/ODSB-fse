2d3ce741f0c82b6b2fa7cf8918b62a0f29a46099
malda motuma <maldamotuma@gmail.com>
2020-01-21 20:11:41 -1200

    final commit

---
+++ odsb/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class admin(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	user_name = models.CharField(max_length=50)
+	email = models.CharField(max_length=100)
+	password = models.TextField()
+class role(models.Model):
+	username = models.CharField(max_length=100)
+	role = models.CharField(max_length=100)
+class Teacher(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	department = models.CharField(max_length=200)
+	email = models.CharField(max_length=200)
+class SP(models.Model):
+	service = models.TextField()
+	email = models.CharField(max_length=200)
+class constore(models.Model):
+	email = models.CharField(max_length=200)
+	confirm = models.IntegerField()
+class Suggestions(models.Model):
+	suggestion_for = models.CharField(max_length=100)
+	given_for = models.CharField(max_length=100)
+	overall = models.CharField(max_length=20)
+	idea = models.TextField()
+	given_time = models.DateTimeField(auto_now=True, auto_now_add=False)
+	giver_id = models.CharField(max_length=20)
+	name = models.CharField(max_length=150)
+	private = models.BooleanField()
+class POST(models.Model):
+	posted_from = models.CharField(max_length=100)
+	poster_id = models.CharField(max_length=100)
+	idea = models.TextField()
+	given_time = models.DateTimeField(auto_now=True, auto_now_add=False)
+	name = models.CharField(max_length=200)
+class student(models.Model):
+	department = models.CharField(max_length=200)
+	year = models.IntegerField()
+	student_id = models.CharField(max_length=20)
+	email = models.CharField(max_length=200)
