70a8932a659c45ef8ecd642b129f3f111218a89c
Abdi Lelisa <abdilelisa41@gmail.com>
2020-01-22 10:19:11 +0300

    model

---
+++ models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class admin(models.Model):
+	
+class role(models.Model):
+	
+class Teacher(models.Model):
+	
+class SP(models.Model):
+	service = models.TextField()
+	email = models.CharField(max_length=200)
+class constore(models.Model):
+	
+class Suggestions(models.Model):
+	
+class POST(models.Model):
+	
+class student(models.Model):
+	
